bd5369dd90ac79d618b996122496e1e6644e176b
Juan Antonio Caja García <juanantonio.caja.contractor@bbva.com>
2017-03-02 03:35:26 +0100

    Two unit tests passing.

--- src/main/java/LexicographicallySorter.java
+++ src/main/java/LexicographicallySorter.java
@@ -1,15 +1,53 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.mockito.internal.util.StringJoiner.join;
 
 public class LexicographicallySorter {
 
 	public String sort(String input) {
-		String expected =
-				"cupfacebookforhackerstudentsstudious\n" +
-						"duzklvrawqrc\n" +
-						"dyroiymybeaxeyubxzdr\n" +
-						"bwjibwjibwjijp\n" +
-						"dcyihopjijliuiuy\n";
+		List<String> result = new ArrayList<>();
+		try (BufferedReader reader = new BufferedReader(new StringReader(input))) {
+			String line = reader.readLine();
+			int number = Integer.parseInt(line);
+			String sortedLine;
 
-		return expected;
+			for (int i = 0; i < number; i++) {
+				line = reader.readLine();
+				sortedLine = lexicographicallySortLine(line);
+				result.add(sortedLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return StringUtils.join(result, "\n");
+	}
+
+	private String lexicographicallySortLine(String line) {
+		String[] words = line.substring(2).split("\\s+");
+
+		Comparator<String> comparator = new Comparator<String>() {
+			@Override
+			public int compare(String a, String b) {
+				return (a + b).compareTo(b + a);
+			}
+		};
+
+		Arrays.sort(words, comparator);
+
+		String result = "";
+		for (String word: words) {
+			result += word;
+		}
+		return result;
 	}
 
 }
--- src/test/java/LexicographicallySortShould.java
+++ src/test/java/LexicographicallySortShould.java
@@ -22,7 +22,57 @@ public class LexicographicallySortShould {
 				"duzklvrawqrc\n" +
 				"dyroiymybeaxeyubxzdr\n" +
 				"bwjibwjibwjijp\n" +
-				"dcyihopjijliuiuy\n";
+				"dcyihopjijliuiuy";
+
+		assertThat(lexicographicallySorter.sort(input), is(expected));
+	}
+
+	@Test
+	public void sortComplexSample() {
+		String input =
+				"20\n" +
+				"9 rgh woqg dmabatgbt qrvpcrx eluunoi sy w wnthqxgkg aimallazuc\n" +
+				"9 jg j uj ujnzdng nzdng nzdn e g ujnzdngj\n" +
+				"5 jibw ji jp bw jibw\n" +
+				"9 nl jtdmdxu ux nlmnyzdxu mnyz jtdm nlmnyz dxu uxdxu\n" +
+				"9 dcn csmzj krnc vkcoume wvpva yqoexwujwp v cxepgptf xb\n" +
+				"9 ksdzsjz bbio ja mvvyxzkmq zgdvxolmt xgvwdbfqzn rhubnqtaad qa eeb\n" +
+				"9 iccrmcrm mwp sil iccrmcrm ic odo iccrm crm odocrm\n" +
+				"9 o zt da wv brorejctww fu phnej ynrdkylwys ekggrmehcl\n" +
+				"9 hdfeax d s uxnnrzko nxpcu v njxqbnh aaqzeeb kxpkw\n" +
+				"9 vkzzfgtobz i tkczlqaf peqmnyoh eogzpbe bgorlllxor bkkupvnqr gsr xfqmteh\n" +
+				"9 wjxwgm qdhmzkmpzv uhibo gcikegpzv ceqiwekdx rxegvkc ujjvbv kfit peiawyk\n" +
+				"9 eavcqvv wyuh mkfq not evhlpur eidqnartht pesgphnnlq t ztvu\n" +
+				"9 z dvqgfh wqx vnajabkqvs sdwkc dlhcnc ezrcvsc teje gzwwj\n" +
+				"9 i hsmh hsmheh xgi eh xg xgeh xnfc ihsmh\n" +
+				"9 izqht h qpbdayaifl pjsoie sujccnm umj dralemrspo euswuti m\n" +
+				"9 r wwwr ndtc ndtclp lpb b wwwb www lp\n" +
+				"9 c evaxdeyrxb rhhfmdm xq vxedern diqs tpdofbc rbq kbxdy\n" +
+				"9 k itqsgpwze ma yhpncg xtf w m kahula zgbo\n" +
+				"9 fujv mzr kgukjmokvz schpxugnef p rjojpzbsro wpobp wl od\n" +
+				"9 u ufmu ufmuqfy vmc ufm uqfy z vmcu qfy";
+
+		String expected =
+				"aimallazucdmabatgbteluunoiqrvpcrxrghsywnthqxgkgwoqgw\n" +
+				"egjgjnzdngnzdnujnzdngjujnzdnguj\n" +
+				"bwjibwjibwjijp\n" +
+				"dxujtdmdxujtdmmnyznlmnyzdxunlmnyznluxdxuux\n" +
+				"csmzjcxepgptfdcnkrncvkcoumevwvpvaxbyqoexwujwp\n" +
+				"bbioeebjaksdzsjzmvvyxzkmqqarhubnqtaadxgvwdbfqznzgdvxolmt\n" +
+				"crmiccrmcrmiccrmcrmiccrmicmwpodocrmodosil\n" +
+				"brorejctwwdaekggrmehclfuophnejwvynrdkylwyszt\n" +
+				"aaqzeebdhdfeaxkxpkwnjxqbnhnxpcusuxnnrzkov\n" +
+				"bgorlllxorbkkupvnqreogzpbegsripeqmnyohtkczlqafvkzzfgtobzxfqmteh\n" +
+				"ceqiwekdxgcikegpzvkfitpeiawykqdhmzkmpzvrxegvkcuhiboujjvbvwjxwgm\n" +
+				"eavcqvveidqnarthtevhlpurmkfqnotpesgphnnlqtwyuhztvu\n" +
+				"dlhcncdvqgfhezrcvscgzwwjsdwkctejevnajabkqvswqxz\n" +
+				"ehhsmhehhsmhihsmhixgehxgixgxnfc\n" +
+				"dralemrspoeuswutihizqhtmpjsoieqpbdayaiflsujccnmumj\n" +
+				"blpblpndtclpndtcrwwwbwwwrwww\n" +
+				"cdiqsevaxdeyrxbkbxdyrbqrhhfmdmtpdofbcvxedernxq\n" +
+				"itqsgpwzekahulakmamwxtfyhpncgzgbo\n" +
+				"fujvkgukjmokvzmzrodprjojpzbsroschpxugnefwlwpobp\n" +
+				"qfyufmufmuqfyufmuuqfyuvmcuvmcz";
 
 		assertThat(lexicographicallySorter.sort(input), is(expected));
 	}
